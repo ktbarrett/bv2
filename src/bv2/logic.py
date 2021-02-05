736b6a2e0e75ab5a3b7bf10bb965f9c9081a5288
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-02-05 00:04:12 -0600

    Optimization

--- src/bv2/logic.py
+++ src/bv2/logic.py
@@ -1,47 +1,24 @@
-from typing import Optional, List, Any
+from typing import Optional, List, Any, Dict
 
 
 class Logic:
 
-    _repr_map = {
-        # 0 and weak 0
-        False: 0,
-        0: 0,
-        "0": 0,
-        "L": 0,
-        "l": 0,
-        # 1 and weak 1
-        True: 1,
-        1: 1,
-        "1": 1,
-        "H": 1,
-        "h": 1,
-        # unknown, unassigned, and weak unknown
-        None: 2,
-        "X": 2,
-        "x": 2,
-        "U": 2,
-        "u": 2,
-        "W": 2,
-        "w": 2,
-        # high impedance
-        "Z": 3,
-        "z": 3,
-    }
-
+    # class tables
+    _constructor_table: Dict[Any, "Logic"]
     _and_table: List[List["Logic"]]
     _or_table: List[List["Logic"]]
     _xor_table: List[List["Logic"]]
     _not_table: List["Logic"]
 
-    def __init__(self, value: Optional[Any] = None):
+    # attributes
+    _repr: int
+
+    def __new__(cls, value: Optional[Any] = None) -> "Logic":
         try:
-            self._repr = self._repr_map[value]
+            return cls._constructor_table[value]
         except KeyError:
             raise ValueError(
-                "{value!r} is not a {self_type}".format(
-                    value=value, self_type=type(self).__name__
-                )
+                "{value!r} is not a {cls}".format(value=value, cls=cls.__name__)
             ) from None
 
     def __and__(self, other: object) -> "Logic":
@@ -96,14 +73,44 @@ class Logic:
         return self._repr
 
 
-Logic._repr_map.update(
-    {
-        Logic("0"): 0,
-        Logic("1"): 1,
-        Logic("X"): 2,
-        Logic("Z"): 3,
-    }
-)
+_0 = object.__new__(Logic)
+_0._repr = 0
+_1 = object.__new__(Logic)
+_1._repr = 1
+_X = object.__new__(Logic)
+_X._repr = 2
+_Z = object.__new__(Logic)
+_Z._repr = 3
+
+Logic._constructor_table = {
+    # 0 and weak 0
+    False: _0,
+    0: _0,
+    "0": _0,
+    "L": _0,
+    "l": _0,
+    _0: _0,
+    # 1 and weak 1
+    True: _1,
+    1: _1,
+    "1": _1,
+    "H": _1,
+    "h": _1,
+    _1: _1,
+    # unknown, unassigned, and weak unknown
+    None: _X,
+    "X": _X,
+    "x": _X,
+    "U": _X,
+    "u": _X,
+    "W": _X,
+    "w": _X,
+    _X: _X,
+    # high impedance
+    "Z": _Z,
+    "z": _Z,
+    _Z: _Z,
+}
 
 Logic._and_table = [
     [Logic(v) for v in "0000"],
